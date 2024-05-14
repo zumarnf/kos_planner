35c26deab378d9aedfafcc9ed06ed276596c9f5c
zumzum <kazonima@gmail.com>
2024-05-14 21:26:03 +0700

    tes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,21 +12,21 @@
 </head>
 <body class="h-full">
 
-<div class="min-h-full">
-    <nav class="bg-gray-800" x-data="{ isOpen: false }">
+  <div class="min-h-full">
+    <nav class="bg-white-green" x-data="{ isOpen: false }">
       <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div class="flex h-16 items-center justify-between">
           <div class="flex items-center">
             <div class="flex-shrink-0">
-              <img class="h-8 w-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company">
+              <a href="#" class="text-2xl font-extrabold text-green-gd">Kos Planner</a>
             </div>
             <div class="hidden md:block">
               <div class="ml-10 flex items-baseline space-x-4">
                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                <a href="/" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
-                <a href="/blog" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Blog</a>
-                <a href="/about" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About</a>
-                <a href="/contact" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Contact</a>
+                <a href="/" class="text-black-gd rounded-md px-3 py-2 text-sm font-bold" aria-current="page">Home</a>
+                <a href="/blog" class="text-black-gd hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-bold">Blog</a>
+                <a href="/about" class="text-black-gd hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-bold">About</a>
+                <a href="/contact" class="text-black-gd hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-bold">Contact</a>
 
               </div>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
-export default {
+module.exports = {
     content: [
         "./resources/**/*.blade.php",
         "./resources/**/*.js",
         "./resources/**/*.vue",
     ],
     theme: {
+        colors: {
+            "white-green": "#fafffb",
+            "black-blue": "#394146",
+            "green-gd": "#78ad87",
+            "grey-gd": "#8d8d8d",
+            "black-gd": "#263238",
+        },
         extend: {
             fontFamily: {
                 sans: ["Inter var", ...defaultTheme.fontFamily.sans],
